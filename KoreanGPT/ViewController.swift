b7fe6c1ce1e2b3b4aae9b0fda80c97a468a8e43f
Colli <happythgus@khu.ac.kr>
2022-08-23 10:40:10 +0900

    start

---
+++ KoreanGPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KoreanGPT
+//
+//  Created by 김소현 on 2022/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
